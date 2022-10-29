34105403fe8428633929e00aa3b06ae281178598
Brennan Benson <brennan@atlasreality.com>
2022-10-29 01:40:10 -0500

    Re-commit master's "Added flags for registration + matching fields"

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ class Config(object):
     #generates a hidden field that includes a token that is used to protect the form against CSRF attacks
     
     #running from external database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     #running locally
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False #dont need a notification every time the database is changed
     #configure app.db database, store it in basedir.
@@ -33,3 +33,9 @@ class Config(object):
     UPLOAD_EXTENSIONS_RESUME = os.environ.get('UPLOAD_EXTENSIONS_RESUME')
     LOG_DATA = os.environ.get('LOG_DATA')
     MAX_CONTENT_LENGTH = int(os.environ.get('MAX_CONTENT_LENGTH'))
+
+
+    #MATCHING FLAGS
+    MATCHING_FLAG_PERSONALITY = os.environ.get('MATCHING_FLAG_PERSONALITY')
+    MATCHING_FLAG_DIVISION_PREFERENCE = os.environ.get('MATCHING_FLAG_DIVISION_PREFERENCE')
+    MATCHING_FLAG_MENTOR_GENDER_PREFERENCE = os.environ.get('MATCHING_FLAG_MENTOR_GENDER_PREFERENCE')
