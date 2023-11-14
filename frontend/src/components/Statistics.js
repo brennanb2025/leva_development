a736194e82f0eb805fca0fb44e7e4fd7ebccc22d
kyeoul <54390304+kyeoul@users.noreply.github.com>
2023-11-13 22:07:30 -0600

    Frontend updates

--- frontend/src/components/Statistics.js
+++ frontend/src/components/Statistics.js
@@ -7,10 +7,11 @@ import Modal from 'react-modal'
 
 function Statistics() {
 
-    const [userResults, setUserResults] = useState()
-    const [eventResults, setEventResults] = useState()
+    const [userResults, setUserResults] = useState([])
+    const [eventResults, setEventResults] = useState([])
     const [deleteResults, setDeleteResults] = useState()
     const [modalOpen, setModalOpen] = useState(false)
+    const [currentQuery, setCurrentQuery] = useState("")
 
     const handleSubmitForm = (values) => {
         axios.get("/csrf", { withCredentials: true }).then((response) => {
@@ -33,6 +34,7 @@ function Statistics() {
     }
 
     function UserModal() {
+        let person = userResults[0]
         return (
             <Modal
                 isOpen={modalOpen}
@@ -42,8 +44,46 @@ function Statistics() {
             >
                 <button onClick={() => { setModalOpen(false) }}>Close</button>
 
-                <div className="w-full h-full flex flex-row">
-                    {JSON.stringify(userResults[0])}
+                <div className='w-full flex flex-col justify-center items-center'>
+                    <img src={person.profile_picture} className="w-28 h-28 rounded-full" />
+                    <div className='matchmaking-subheader mb-4'>{person.first_name} {person.last_name}</div>
+                    <div className="text-start">
+                        {
+                            Object.keys(person).map((k, i) => {
+                                if (k === "profile_picture"
+                                    || k === "first_name"
+                                    || k === "last_name"
+                                    || !person[k]) {
+                                    return ""
+                                }
+                                else if (k === "resume") {
+                                    return <a href={person[k]} className='text-blue-700 underline'>Resume</a>
+                                }
+                                else if (typeof person[k] == "object") {
+                                    return (
+                                        <div>
+                                            <span className='font-bold'>{k}: </span>
+                                            <span>{
+                                                person[k].map((field, i) => {
+                                                    if (i == person[k].length - 1) {
+                                                        return field
+                                                    }
+                                                    return field + ", "
+                                                })
+                                            }</span>
+                                        </div>
+                                    )
+                                }
+
+                                return (
+                                    <div>
+                                        <span className='font-bold'>{k}: </span>
+                                        <span>{person[k]}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </Modal>
         )
@@ -88,6 +128,7 @@ function Statistics() {
                         }).then((results) => {
                             setUserResults(results.data)
                         })
+                        setCurrentQuery(values.picked)
                     }}>
                     {({ values }) => (
                         <Form>
@@ -139,7 +180,7 @@ function Statistics() {
 
                             <button type="submit" className='submit-button'>Submit</button>
 
-                            {userResults && values.picked === "lookup" ? (
+                            {userResults.length > 0 && currentQuery === "lookup" ? (
                                 <div className='bg-slate-500 rounded-md p-4 text-white mt-4 w-fit flex flex-row items-center hover:cursor-pointer'
                                     onClick={() => { setModalOpen(true) }}>
                                     <img src={userResults[0].profile_picture} className='h-16 w-auto rounded-full' />
@@ -158,7 +199,16 @@ function Statistics() {
                                 </div>
                             ) : ""}
 
-                            {userResults ? <UserModal /> : ""}
+                            {userResults.length > 0 && currentQuery != "lookup" ? (
+                                <div className='bg-slate-500 rounded-md p-4 text-white mt-4 w-fit flex flex-row items-center hover:cursor-pointer'
+                                >
+                                    {
+                                        JSON.stringify(userResults)
+                                    }
+                                </div>
+                            ) : ""}
+
+                            {userResults.length > 0 ? <UserModal /> : ""}
                         </Form>
                     )}
                 </Formik>
@@ -171,6 +221,7 @@ function Statistics() {
                 </div>
                 <Formik
                     initialValues={{
+                        query_type: "16",
                         startDate: '',
                         startTime: '',
                         endDate: '',
@@ -179,7 +230,7 @@ function Statistics() {
                     onSubmit={async (values) => {
                         axios.get("/admin-events-exceptions", {
                             params: {
-                                "action": 16,
+                                "action": parseInt(values.query_type),
                                 "startTime": values.startDate.toISOString(),
                                 "endTime": values.endDate.toISOString(),
                             },
@@ -194,6 +245,48 @@ function Statistics() {
                         } = props;
                         return (
                             <Form>
+                                <div role="group" aria-labelledby="my-radio-group" className='flex flex-col mt-4'>
+
+                                    {/* "Actions" - the number assigned to each event logged
+                                    Exceptions (16)
+                                    Edit profile picture failure (18)
+                                    Csrf error (17)
+                                    Feed info (13)
+                                    Chosen person feed info (14)
+                                    Uploading file to s3 (8)
+                                    Number of users per day (logins - 4 - must separate by timestamp, so this may require its own function). */}
+
+                                    <label className='regtext'>
+                                        Exceptions
+                                        <Field type="radio" name="query_type" value="16" className="radio-button" />
+                                        <span className="checkmark"></span>
+                                    </label>
+                                    <label className='regtext'>
+                                        Edit profile picture failure
+                                        <Field type="radio" name="query_type" value="18" className="radio-button" />
+                                        <span className="checkmark"></span>
+                                    </label>
+                                    <label className='regtext'>
+                                        Csrf error
+                                        <Field type="radio" name="query_type" value="17" className="radio-button" />
+                                        <span className="checkmark"></span>
+                                    </label>
+                                    <label className='regtext'>
+                                        Feed info
+                                        <Field type="radio" name="query_type" value="13" className="radio-button" />
+                                        <span className="checkmark"></span>
+                                    </label>
+                                    <label className='regtext'>
+                                        Chosen person feed info
+                                        <Field type="radio" name="query_type" value="14" className="radio-button" />
+                                        <span className="checkmark"></span>
+                                    </label>
+                                    <label className='regtext'>
+                                        Uploading file to s3
+                                        <Field type="radio" name="query_type" value="8" className="radio-button" />
+                                        <span className="checkmark"></span>
+                                    </label>
+                                </div>
                                 <div className='flex flex-row'>
                                     <div className='input-container'>
                                         <label htmlFor="startDate">Start Date</label>
@@ -213,12 +306,35 @@ function Statistics() {
                                 </div>
 
                                 <button type="submit" className='submit-button'>Submit</button>
+
+                                {eventResults.length > 0 ? (
+                                    <div className='flex flex-row flex-wrap h-72 overflow-y-scroll mt-2 p-2 border'>
+                                        {
+                                            eventResults.map((error) => {
+                                                return (
+                                                    <div className="basis-1/4  text-white p-4">
+                                                        <div className='bg-slate-500 rounded-md p-2'>
+                                                            {
+                                                                Object.keys(error).map((k, i) => {
+                                                                    return (
+                                                                        <div>
+                                                                            <span className='font-bold'>{k}: </span>
+                                                                            <span>{error[k]}</span>
+                                                                        </div>
+                                                                    )
+                                                                })
+                                                            }
+                                                        </div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                ) : ""}
                             </Form>
                         )
                     }}
                 </Formik>
-
-                <div>{JSON.stringify(eventResults)}</div>
             </section>
 
             <section id='match'>
